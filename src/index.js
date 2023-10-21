21a5794518e640e1f1be815585d3609e72e006bb
Anki <sainiankitas121@gmail.com>
2023-10-21 22:52:13 +0530

    Routes Added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser")
+const ApiRoutes = require('./Routes/index')
 
 const setupAndStartServer = async() => {
     const app = express();
@@ -7,10 +8,11 @@ const setupAndStartServer = async() => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.use('/api' , ApiRoutes)
 
     const PORT = 3000;
 
-    app.listen(PORT , () => {
+    app.listen(PORT , async () => {
         console.log("Hello");
     });
 }
